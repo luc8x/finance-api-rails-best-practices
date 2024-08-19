574a071164dc74bf8f92869da952338df551b984
Luc8x <98758844+luc8x@users.noreply.github.com>
2024-08-19 16:17:57 -0300

    Revert "Feature/setup api configuration"

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,17 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
-require 'action_view/railtie'
-require 'action_cable/engine'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -41,16 +39,6 @@ module FinanceApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.generators do |generator|
-      generator.test_framework :rspec, fixture: true
-      generator.fixture_replacement :factory_bot, dir: 'spec/factories'
-      generator.view_specs false
-      generator.helper_specs false
-      generator.stylesheets = false
-      generator.javascripts = false
-      generator.helper = false
-    end
-    config.eager_load_paths << Rails.root.join('lib')
     config.api_only = true
   end
 end
